4dfc5e01ae10f63a70b2cc92ac029537721e0496
felixfern <felixzhuang1@gmail.com>
2023-12-06 16:37:29 +0700

    fix: firebase admin creds

--- src/gcloud/firebase.ts
+++ src/gcloud/firebase.ts
@@ -22,8 +22,7 @@ export const firebaseAdminCreds = {
 	auth_url: "https://accounts.google.com/o/oauth2/auth",
 	token_uri: "https://oauth2.googleapis.com/token",
 	auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-	client_x509_cert_url:
-		"https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-z96r6%40practice-capstone.iam.gserviceaccount.com",
+	client_x509_cert_url: env.FIREBASE_ADMIN_CERT_URL,
 	universe_domain: "googleapis.com",
 };
 
