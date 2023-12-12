30c6cb17ed960b1de6c6fc36826ae8fb65b320fb
felixfern <felixzhuang1@gmail.com>
2023-12-12 12:38:23 +0700

    feat: chat rooms work

--- src/service/socket.ts
+++ src/service/socket.ts
@@ -12,16 +12,27 @@ export const socketConnection = (server: HTTPServer) => {
 	});
 
 	io.on("connection", (socket: any) => {
-		console.log(`User Connected : ${socket.id}`);
-
 		socket.on("send_message", (data: any) => {
 			messages = [...messages, { message: data, id: socket.id }];
+			if (data.room) {
+				socket.to(data.room).emit("receive_message", messages);
+			} else {
+				socket.broadcast.emit("receive_message", messages);
+			}
 			socket.emit("receive_message", messages);
-			socket.broadcast.emit("receive_message", messages);
 		});
 
 		socket.on("receive_message", () => {
-			socket.emit("receive_message", messages);
+			if (socket.room) {
+				socket.to(socket.room).emit("receive_message", messages);
+			} else {
+				socket.broadcast.emit("receive_message", messages);
+			}
+		});
+
+		socket.on("join_room", (room_id: string) => {
+			console.log(`joined ${room_id}`);
+			socket.join(room_id);
 		});
 	});
 };
