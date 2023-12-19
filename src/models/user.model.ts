bfbe139189ed5b369c110cba3c7e924741187a9c
Felix Fernando <felixzhuang1@gmail.com>
2023-12-19 11:14:11 +0700

    [Feature] User API (2/2) (#21)
    * feat: added update user profile, upload user profile image, and fix upload file function
    
    * chore: remove lodash

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -8,3 +8,20 @@ export const UserSchema = z.object({
 });
 
 export type User = z.infer<typeof UserSchema>;
+
+const phoneRegex = new RegExp(
+	/^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/
+);
+
+const dobRegex = new RegExp(
+	"^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\\d{4}$"
+);
+export const UpdateUserSchema = z.object({
+	email: z.string().email({ message: "Invalid email format" }),
+	name: z.string(),
+	dob: z.string().regex(dobRegex, "Invalid Date (DD/MM/YYYY)"),
+	phoneNumber: z.string().regex(phoneRegex, "Invalid Number"),
+	location: z.string(),
+});
+
+export type UpdateUser = z.infer<typeof UpdateUserSchema>;
